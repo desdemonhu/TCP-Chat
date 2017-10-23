1e98a2216c64f6c71fec15bd2bcf16e8c9b5766f
Raegan Millhollin <desdemonhu@gmail.com>
2017-10-23 13:04:26 -0700

    Server listens on port 3000

--- server.js
+++ server.js
@@ -4,14 +4,17 @@ const net = require('net');
 const port = 3000;
 const server = net.createServer();
 
+server.listen(port, ()=>{
+    console.log("Listening on port: ", port);
+  });
+
 server.on('connection', (socket) => {
   //assign username
   //socketPool
-  console.log(port);
+
   server.on('data', (buffer) => {
     ///buffer has text.toString
   })
 
   /// .write to write to socketPool
-
 });
