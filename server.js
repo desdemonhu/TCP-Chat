479fe259aa26af3a8360c4291c475468148ef4e1
Raegan Millhollin <desdemonhu@gmail.com>
2017-10-24 08:21:36 -0700

    Added console logs to createServer

--- server.js
+++ server.js
@@ -2,19 +2,30 @@
 
 const net = require('net');
 const port = 3000;
-const server = net.createServer();
+const server = net.createServer((client) => {
+  console.log('Client Connected');
+  client.on('end', () => {
+    console.log('Client disconnected.');
+  })
 
-server.listen(port, ()=>{
-    console.log("Listening on port: ", port);
-  });
+  client.write('hello\r\n');
+  client.pipe(client);
+});
+
 
 server.on('connection', (socket) => {
   //assign username
   //socketPool
+  console.log(socket);
 
-  server.on('data', (buffer) => {
+  socket.on('data', (buffer) => {
     ///buffer has text.toString
+    let text = buffer.toString();
+    console.log(text);
   })
 
   /// .write to write to socketPool
 });
+server.listen(port, () => {
+    console.log("Listening on port: ", port);
+  });
