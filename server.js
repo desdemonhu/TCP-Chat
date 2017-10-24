b75f1e8021c099301afa8acdd02e3bcf3d7aafdf
Raegan Millhollin <desdemonhu@gmail.com>
2017-10-24 16:39:49 -0700

    dm works!

--- server.js
+++ server.js
@@ -17,17 +17,42 @@ const server = net.createServer((client) => {
 server.on('connection', (socket) => {
   //assign username
   //socketPool
-  connection.clientCreate(socket);
+
   console.log(connection.socketPool);
   let textString = '';
+
+  let socketIndex = connection.clientCreate(socket);
 
   socket.on('data', (buffer) => {
     ///buffer has text.toString
     let text = buffer.toString();
     textString += text;
 // \u0003  \u0085  \u000D
-    if(text == '\u0085'){
+    if(text == '\r\n'){
       console.log(textString);
+
+      if(textString.startsWith('@')){
+        let command = textString.slice(1).split(' ');
+        console.log(command);
+        let commandIndex = connection.commands.map(function(e){
+          return e.name
+        }).indexOf(command[0]);
+
+        console.log(commandIndex);
+        if(commandIndex > -1){
+          connection.commands[commandIndex].callback(socketIndex, textString);
+          textString = '';
+        }else {
+          socket.write('command not found');
+          socket.write(`Available commands are: ${connection.commands}`);
+          textString = '';
+        }
+      } else {
+        connection.socketPool.forEach((client) => {
+          client.socket.write(`${client.nickname}: ${textString}`);
+          textString = '';
+        })
+      }
     }
 
   })
