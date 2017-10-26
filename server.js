3c5dfb6ee4b84c98fcd214577b74fb52d6713168
Raegan Millhollin <desdemonhu@gmail.com>
2017-10-26 10:00:29 -0700

    required OS module so that end of lines are compatable accross platforms

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const connection = require('./lib/socket.js');
+const os = require('os');
 
 const net = require('net');
 const port = 3000;
@@ -10,7 +11,7 @@ const server = net.createServer((client) => {
     console.log('Client disconnected.');
   })
 
-  client.write('Welcome to my world\r\n\r\n');
+  client.write(`Welcome to my world'${os.EOL}${os.EOL}`);
 });
 
 
@@ -26,11 +27,11 @@ server.on('connection', (socket) => {
     textString += text;
 
 ///When somene hits return start checking if it's a command or send data
-    if(text == '\r\n'){
+    if(text == os.EOL){
       console.log(`${socketIndex}: ${textString}`);
 
       if(textString.startsWith('@')){
-        let command = textString.slice(1).replace('\r\n', '').split(' ');
+        let command = textString.slice(1).replace(os.EOL, '').split(' ');
         console.log(`${connection.socketPool[socketIndex].nickname}:  ${command}`);
 
         ///Looks for command in the command list
@@ -43,7 +44,7 @@ server.on('connection', (socket) => {
           connection.commands[commandIndex].callback(socketIndex, textString);
           textString = '';
         }else {
-          socket.write('command not found\r\n');
+          socket.write(`command not found${os.EOL}`);
           ///Bring up help menu
           connection.commands[0].callback(socketIndex,text);
 
